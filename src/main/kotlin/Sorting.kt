c3c8efdfc7b91c2f819e8e24811bd0f093ac1b0f
islamdidarmd <islamdidarmd@gmail.com>
2021-01-12 14:43:29 +0600

    Added Bubble Sort

---
+++ src/main/kotlin/Sorting.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val arr = readLine()!!.split(" ").map(String::toInt)
+    bubbleSort(arr.toMutableList())
+}
+
+fun bubbleSort(arr: MutableList<Int>) {
+    for (i in 0..arr.size - 2) {
+        for (j in i + 1 until arr.size) {
+            if (arr[i] > arr[j]) {
+                val temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+
+    println("-------Bubble Sort--------")
+    println(arr)
+}
