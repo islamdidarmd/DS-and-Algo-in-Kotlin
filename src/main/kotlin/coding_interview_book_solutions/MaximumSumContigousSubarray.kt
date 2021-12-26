6df90b8f4d07f796fc1289dead599d44c3bfdf25
Md Didarul islam <islamdidarmd@gmail.com>
2021-12-26 19:25:29 +0600

    Add Maximum Contiguous Sub Array

---
+++ src/main/kotlin/coding_interview_book_solutions/MaximumSumContigousSubarray.kt
@@ -0,0 +1,19 @@
+package coding_interview_book_solutions
+
+fun main() {
+    val nums = readLine()!!.split(" ").map(String::toInt)
+
+    var maxSum = Int.MIN_VALUE
+    var currentSum = 0
+
+    for (i in nums.indices) {
+        currentSum += nums[i]
+        if(currentSum > maxSum){
+            maxSum = currentSum
+        }
+
+        if(currentSum < 0) currentSum = 0
+    }
+
+    println(maxSum)
+}
